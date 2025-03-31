20b0869af84d48b6db49377b1505107507851ba8
sureshannepu12 <suresh.annepu@ateliasoftware.com>
2025-03-31 09:56:28 +0530

    removed comments

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-// Fetch all employees
 export const fetchEmployees = async () => {
   try {
     const response = await axios.get(API_URL);
